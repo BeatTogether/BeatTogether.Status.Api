3d70aec8ff374d2656e0fb6cfc42fd398d266996
cubic <cubicgraphics98@gmail.com>
2022-06-02 13:51:03 +0100

    Added a list of TODOs

--- Configuration/MasterserverConfiguration.cs
+++ Configuration/MasterserverConfiguration.cs
@@ -7,5 +7,7 @@ namespace BeatTogether.Api.Configuration
         public string Endpoint { get; set; } = "BeatTogether.SomeIPAddress";
         public string FullAccess { get; }  = "FULLACCESS"; //TODO Change this when put on main
         public string CreateAndDestryPermanentServers { get; } = "MIDACCESS"; //TODO change this when put on main
+        //TODO make a better system for AccessTokens, eg loading a file into a data dictionary of access token to AllowedRequests
+        //TODO cache values recieved from the master and dedi servers and only fetch from master or dedi if the last request was over 2 or 1 seconds ago to increase response timings if there are potentialy alot of users making many requests at once and reduce server lag.
     }
 }
