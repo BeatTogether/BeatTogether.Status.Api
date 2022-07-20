55d02d9c0892624fc5139c34e28558e4aea9869e
goobwabber <littlehappytroll@gmail.com>
2022-07-19 22:20:55 -0400

    info endpoint

---
+++ Configuration/InfoConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace BeatTogether.Status.Api.Configuration
+{
+    public class InfoConfiguration
+    {
+        public string ServerDisplayName { get; set; } = string.Empty;
+        public string ServerDescription { get; set; } = string.Empty;
+        public string ServerImageUrl { get; set; } = string.Empty;
+    }
+}
---
+++ Models/MasterServerInfoData.cs
@@ -0,0 +1,7 @@
+﻿namespace BeatTogether.Status.Api.Models
+{
+    public record MasterServerInfoData(
+        string name,
+        string description, 
+        string imageUrl);
+}
